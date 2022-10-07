b7aa7fc45103057eaf8f08e593ddeeb15197d385
Aleksey <fosterfosterfoster63@gmail.com>
2022-10-07 21:26:31 +0400

    try fix footer

--- components/main/video/index.vue
+++ components/main/video/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <video ref="mainVideo" muted loop playsinline webkit-playinginline id="mainVideo" src="@/assets/video/mainvideomp4.mp4"></video>
+  <video ref="mainVideo" muted loop playsinline webkit-playinginline id="mainVideo">
+    <source src="@/assets/video/mainvideo.webm" type="video/webm">
+    <source src="@/assets/video/mainvideomp4.mp4" type="video/mp4">
+  </video>
 </template>
