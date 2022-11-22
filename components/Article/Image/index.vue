f775e5928aa4f18f3f8c4a72bf23dbee27ba05b1
Aleksey <fosterfosterfoster63@gmail.com>
2022-11-22 16:52:42 +0400

    fix container

--- components/Article/Image/index.vue
+++ components/Article/Image/index.vue
@@ -1,12 +1,10 @@
 <template>
-  <ui-container class="noPaddingTop">
-    <div class="">
-      <div :class="$style.mainImage">
-        <!-- <img :src="mainImage" alt=""> -->
-        <nuxt-img loading="lazy" v-if="mainImage" :src="mainImage" />
-      </div>
+  <div class="">
+    <div :class="$style.mainImage">
+      <!-- <img :src="mainImage" alt=""> -->
+      <nuxt-img loading="lazy" v-if="mainImage" :src="mainImage" />
     </div>
-  </ui-container>
+  </div>
 </template> 
 <script src="./setup.js"></script>
 <style lang="scss" scoped module src="./style.scss"></style>
