062854c9e3df7e89ea28422adbb080c1893536ea
Aleksey <fosterfosterfoster63@gmail.com>
2022-11-22 15:44:20 +0400

    change text and blocks

--- components/Article/Video/index.vue
+++ components/Article/Video/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <ui-container v-if="hasVideo" class="noPaddingTop">
-    <div :class="[
-      $style.mainVideo,
-      $props.order === 0 ? $style.fullWidth : ''
-      ]">
-      <video ref="mainVideo" controls :poster="hasPoster ? mainVideoPoster : ''" loop playsinline webkit-playsinline webkit-playinginline id="mainVideo">
-        <source poster="https://koshta.c.roky.rocks/uploads/mainposter_ba70b0b94a.png?updated_at=2022-10-19T17:58:47.272Z" :src="mainVideo" type="video/mp4">
-      </video>
-    </div>
-  </ui-container>
+  <div v-if="hasVideo" :class="[
+    $style.mainVideo,
+    $props.order === 0 ? $style.fullWidth : ''
+    ]">
+    <video ref="mainVideo" controls :poster="hasPoster ? mainVideoPoster : ''" loop playsinline webkit-playsinline webkit-playinginline id="mainVideo">
+      <source poster="https://koshta.c.roky.rocks/uploads/mainposter_ba70b0b94a.png?updated_at=2022-10-19T17:58:47.272Z" :src="mainVideo" type="video/mp4">
+    </video>
+  </div>
 </template> 
 <script src="./setup.js"></script>
 <style lang="scss" scoped module src="./style.scss"></style>
