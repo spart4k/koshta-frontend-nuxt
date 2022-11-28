2d73fc139c2daa882bd74db0051fc9b8f8b1a23a
Aleksey <fosterfosterfoster63@gmail.com>
2022-11-28 10:47:15 +0400

    try fix manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest', crossorigin: "use-credentials" },
+      { rel: 'manifest', href: '/manifest.json', crossorigin: "use-credentials" },
       { rel: 'mask-icon', color: "#5BBAD5", href: '/safari-pinned-tab.svg' }
     ],
     script: [
